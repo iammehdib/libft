b7f00036d80c7a6024046d2a857ca62891eed1fb
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-03-21 00:00:49 +0100

    feat(ft_memcpy): init code

---
+++ ft_memcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/20 23:16:29 by mbuchet           #+#    #+#             */
+/*   Updated: 2026/03/20 23:55:31 by mbuchet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <string.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t				index;
+	unsigned char		*dest_unsigned;
+	const unsigned char	*src_unsigned;
+
+	index = 0;
+	dest_unsigned = (unsigned char *)dest;
+	src_unsigned = (const unsigned char *)src;
+	while (index < n)
+	{
+		dest_unsigned[index] = src_unsigned[index];
+		index++;
+	}
+	return (dest);
+}
