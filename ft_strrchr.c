d0279f532ed5cda24139d03f9cfe47032a8e4514
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-03-21 22:15:01 +0100

    feat(ft_strrchr): init code

---
+++ ft_strrchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/21 19:25:37 by mbuchet           #+#    #+#             */
+/*   Updated: 2026/03/21 22:14:31 by mbuchet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <string.h>
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t				index;
+	const unsigned char	*str_unsigned;
+	unsigned char		char_unsigned;
+
+	index = ft_strlen(s);
+	str_unsigned = (const unsigned char *) s;
+	char_unsigned = (unsigned char) c;
+	while (index != 0)
+	{
+		if (str_unsigned[index] == char_unsigned)
+			return ((char *) &str_unsigned[index]);
+		index--;
+	}
+	return (NULL);
+}
