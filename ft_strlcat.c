724c092d0838ceb17c9ce6ae4d1b287eb6b68794
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-03-21 19:24:34 +0100

    fix(ft_strlcat): fix dst_length calcul

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,13 +6,12 @@
 /*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 13:25:05 by mbuchet           #+#    #+#             */
-/*   Updated: 2026/03/21 18:38:40 by mbuchet          ###   ########.fr       */
+/*   Updated: 2026/03/21 19:00:38 by mbuchet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "string.h"
 #include <stddef.h>
-#include <stdio.h>
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dsize)
@@ -22,7 +21,9 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dsize)
 	size_t	src_length;
 
 	index = 0;
-	dst_length = ft_strlen(dst);
+	dst_length = 0;
+	while (dst[dst_length] && dst_length < dsize)
+		dst_length++;
 	src_length = ft_strlen(src);
 	if (dst_length >= dsize)
 		return (dsize + src_length);
