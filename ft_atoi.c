41ce9e248a69aeca44466b2eb2275b4208d36323
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-03-22 00:08:52 +0100

    feat(ft_atoi): init code

---
+++ ft_atoi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/21 23:44:37 by mbuchet           #+#    #+#             */
+/*   Updated: 2026/03/22 00:06:35 by mbuchet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *nptr)
+{
+	int	index;
+	int	sign;
+	int	result;
+
+	index = 0;
+	sign = 1;
+	result = 0;
+	while (nptr[index] == ' ' && (nptr[index] >= 9 && nptr[index] <= 13))
+		index++;
+	if (nptr[index] == '-')
+		sign = -1;
+	while (nptr[index] >= '0' && nptr[index] <= '9')
+	{
+		result = (result * 10) + (nptr[index] - '0');
+		index++;
+	}
+	return (result * sign);
+}
