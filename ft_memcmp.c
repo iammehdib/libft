46626d337241e672f58ef8c785ce7f7f50b8314a
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-03-21 23:03:17 +0100

    feat(ft_memcmp): init code

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/21 22:52:47 by mbuchet           #+#    #+#             */
+/*   Updated: 2026/03/21 23:00:36 by mbuchet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t				index;
+	const unsigned char	*s1_unsigned;
+	const unsigned char	*s2_unsigned;
+
+	index = 0;
+	s1_unsigned = (const unsigned char *) s1;
+	s2_unsigned = (const unsigned char *) s2;
+	while (index < n)
+	{
+		if (s1_unsigned[index] != s2_unsigned[index] || s1_unsigned[index] == 0)
+			return (s1_unsigned[index] - s2_unsigned[index]);
+		index++;
+	}
+	return (0);
+}
