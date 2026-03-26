ebf3cf850f349cd22644f28d79141fba45a506d2
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-03-26 05:30:29 +0100

    feat(ft_putnbr_fd.c): init code

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/26 05:22:29 by mbuchet           #+#    #+#             */
+/*   Updated: 2026/03/26 05:29:44 by mbuchet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	n_long;
+
+	n_long = n;
+	if (n_long < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n_long = -n_long;
+	}
+	if (n_long >= 10)
+		ft_putnbr_fd((int) n_long / 10, fd);
+	ft_putchar_fd('0' + (n_long % 10), fd);
+}
