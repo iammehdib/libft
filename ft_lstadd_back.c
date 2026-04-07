3e322b999e96a77c4c126ee823c75722f9ba46ba
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-04-07 14:46:40 +0200

    feat(ft_lstadd_back): init code

---
+++ ft_lstadd_back.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/26 19:32:14 by mbuchet           #+#    #+#             */
+/*   Updated: 2026/04/07 14:30:37 by mbuchet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    ft_lstadd_back(t_list **lst, t_list *new)
+{
+    t_list    *last_lst;
+    t_list    *linked_list;
+
+    if (lst == NULL || new == NULL)
+        return ;
+    linked_list = *lst;
+    if (linked_list == NULL)
+        return ;
+    while (linked_list)
+    {
+        last_lst = linked_list;
+        linked_list = linked_list->next;
+    }
+    new->next = NULL;
+    *lst = new;
+}
