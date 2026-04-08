f973ad99abe1bf245ccf7ee36e066aea3d6a5a75
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-04-08 11:21:13 +0200

    refactor(ft_lstadd_back): remove useless linked_list egal to NULL for cancel the func

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 19:32:14 by mbuchet           #+#    #+#             */
-/*   Updated: 2026/04/08 11:19:18 by mbuchet          ###   ########.fr       */
+/*   Updated: 2026/04/08 11:20:12 by mbuchet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	if (lst == NULL || new == NULL)
 		return ;
 	linked_list = *lst;
-	if (linked_list == NULL)
-		return ;
 	while (linked_list)
 	{
 		last_lst = linked_list;
