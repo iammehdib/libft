9782a691dc2ac9b7c715277a39cf0f5ee305878e
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-04-08 15:26:35 +0200

    feat(ft_lstmap): init code

---
+++ ft_lstmap.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/08 15:15:52 by mbuchet           #+#    #+#             */
+/*   Updated: 2026/04/08 15:26:22 by mbuchet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*linked_list;
+	t_list	*current_linked_list;
+
+	if (lst == NULL || f == NULL || del == NULL)
+		return (lst);
+	linked_list = lst;
+	while (current_linked_list)
+	{
+		current_linked_list = ft_lstnew(current_linked_list->content);
+		if (current_linked_list == NULL)
+		{
+			ft_lstdelone(current_linked_list, del);
+			return (NULL);
+		}
+		current_linked_list = current_linked_list->next;
+	}
+	return (linked_list);
+}
