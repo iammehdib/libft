97497e1025d0cfeb2652529b328e4c9680db9adc
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-04-08 11:19:57 +0200

    fix(ft_lstadd_back): replace all possible space to TAB for fix norminette

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,27 +6,27 @@
 /*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 19:32:14 by mbuchet           #+#    #+#             */
-/*   Updated: 2026/04/07 14:30:37 by mbuchet          ###   ########.fr       */
+/*   Updated: 2026/04/08 11:19:18 by mbuchet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-    t_list    *last_lst;
-    t_list    *linked_list;
+	t_list	*last_lst;
+	t_list	*linked_list;
 
-    if (lst == NULL || new == NULL)
-        return ;
-    linked_list = *lst;
-    if (linked_list == NULL)
-        return ;
-    while (linked_list)
-    {
-        last_lst = linked_list;
-        linked_list = linked_list->next;
-    }
-    new->next = NULL;
-    *lst = new;
+	if (lst == NULL || new == NULL)
+		return ;
+	linked_list = *lst;
+	if (linked_list == NULL)
+		return ;
+	while (linked_list)
+	{
+		last_lst = linked_list;
+		linked_list = linked_list->next;
+	}
+	new->next = NULL;
+	*lst = new;
 }
