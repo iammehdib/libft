6b57db7eeb16bd225cbc95c330ddc9cd8812c9b6
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-04-10 18:20:27 +0200

    refactor(ft_lstnew): replace [0] to ->

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 14:13:08 by mbuchet           #+#    #+#             */
-/*   Updated: 2026/03/26 14:23:26 by mbuchet          ###   ########.fr       */
+/*   Updated: 2026/04/10 18:18:57 by mbuchet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_list	*ft_lstnew(void *content)
 	linked_list = malloc(sizeof(t_list));
 	if (linked_list == NULL)
 		return (NULL);
-	linked_list[0].content = content;
-	linked_list[0].next = NULL;
+	linked_list->content = content;
+	linked_list->next = NULL;
 	return (linked_list);
 }
