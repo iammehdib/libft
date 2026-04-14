4c81afb57dc9ce6bc34fa78b0aa1bb59c36e0932
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-04-14 08:08:47 +0200

    fix(ft_itoa): remove useless function and make code work perfectly

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 02:55:00 by mbuchet           #+#    #+#             */
-/*   Updated: 2026/03/26 01:02:37 by mbuchet          ###   ########.fr       */
+/*   Updated: 2026/04/14 08:07:15 by mbuchet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,22 +35,6 @@ static int	get_itoa_malloc_size(int n)
 	return (index);
 }
 
-static void	reverse_str(char *str)
-{
-	int		start;
-	int		end;
-	char	temp_char;
-
-	start = 0;
-	end = ft_strlen(str);
-	while (end > start)
-	{
-		temp_char = str[start];
-		str[start++] = str[--end];
-		str[end] = temp_char;
-	}
-}
-
 static char	*get_zero_result_from_itoa(void)
 {
 	char	*str;
@@ -73,18 +57,18 @@ char	*ft_itoa(int n)
 	str = malloc(sizeof(char) * (get_itoa_malloc_size(n) + 1));
 	if (str == NULL)
 		return (NULL);
-	index = 0;
+	index = get_itoa_malloc_size(n);
+	str[index] = 0;
+	index--;
 	if (n_long < 0)
 	{
-		str[index++] = '-';
+		str[0] = '-';
 		n_long = -n_long;
 	}
 	while (n_long > 0)
 	{
-		str[index++] = '0' + (n_long % 10);
+		str[index--] = '0' + (n_long % 10);
 		n_long /= 10;
 	}
-	str[index] = 0;
-	reverse_str(str);
 	return (str);
 }
