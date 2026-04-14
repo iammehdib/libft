9db6f38b9f62e72a3b530bd36860949015eddb9a
Mehdi Buchet <mbuchet@student.42belgium.be>
2026-04-14 02:33:41 +0200

    fix(ft_putnbr_fd): remove useless int casting

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mbuchet <mbuchet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 05:22:29 by mbuchet           #+#    #+#             */
-/*   Updated: 2026/03/26 05:29:44 by mbuchet          ###   ########.fr       */
+/*   Updated: 2026/04/14 02:33:28 by mbuchet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ void	ft_putnbr_fd(int n, int fd)
 		n_long = -n_long;
 	}
 	if (n_long >= 10)
-		ft_putnbr_fd((int) n_long / 10, fd);
-	ft_putchar_fd('0' + (n_long % 10), fd);
+		ft_putnbr_fd(n_long / 10, fd);
+	ft_putchar_fd((n_long % 10) + '0', fd);
 }
